1f430c77c73f5e11e45bb3d0e17505b128fe1aac
Vita <vitatriutami@gmail.com>
2024-12-21 15:15:02 +0700

    add: fix year format

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // Do your work here...
-console.log('Hello, world!');
+console.log("Hello, world!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  flatpickr("#bookFormYear", {
+    dateFormat: "Y", // Format hanya tahun
+    plugins: [
+      new flatpickr.plugins.yearSelectPlugin({
+        start: 1900, // Tahun awal
+        end: new Date().getFullYear(), // Tahun akhir (tahun ini)
+      }),
+    ],
+  });
+});
