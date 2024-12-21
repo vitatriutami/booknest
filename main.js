223f53ca12f0c025debb32eac47e3e8eb216b93e
Vita <vitatriutami@gmail.com>
2024-12-22 05:58:12 +0700

    add: fix year input

--- main.js
+++ main.js
@@ -35,10 +35,18 @@ function createBookElement(book) {
   authorElement.textContent = `Penulis: ${author}`;
 
   const yearElement = document.createElement("p");
-  authorElement.textContent = `Tahun: ${year}`;
+  yearElement.textContent = `Tahun: ${year}`;
 
   const container = document.createElement("div");
-  container.classList.add("book-item", "bg-slate-200", "rounded-lg", "p-2", "flex", "flex-col", "gap-1");
+  container.classList.add(
+    "book-item",
+    "bg-slate-200",
+    "rounded-lg",
+    "p-2",
+    "flex",
+    "flex-col",
+    "gap-1"
+  );
   container.append(titleElement, authorElement, yearElement);
   container.setAttribute("data-id", id);
 
@@ -47,7 +55,13 @@ function createBookElement(book) {
 
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Hapus";
-  deleteButton.classList.add("px-2", "bg-red-500", "text-white", "rounded-2xl"); 
+  deleteButton.classList.add(
+    "px-2",
+    "bg-red-500",
+    "text-white",
+    "rounded-2xl",
+    "hover:text-black"
+  );
   deleteButton.addEventListener("click", () => {
     removeBook(id);
   });
@@ -59,10 +73,11 @@ function createBookElement(book) {
     markCompleteButton.textContent = "Selesai";
     markCompleteButton.classList.add(
       "px-2",
-      "bg-slate-500",
+      "bg-emerald-500",
       "text-white",
-      "rounded-2xl"
-    ); 
+      "rounded-2xl",
+      "hover:text-black"
+    );
     markCompleteButton.addEventListener("click", () => {
       markBookAsCompleted(id);
     });
@@ -70,6 +85,13 @@ function createBookElement(book) {
   } else {
     const markUncompleteButton = document.createElement("button");
     markUncompleteButton.textContent = "Belum Selesai";
+    markUncompleteButton.classList.add(
+      "px-2",
+      "bg-slate-500",
+      "text-white",
+      "rounded-2xl",
+      "hover:text-rose-300"
+    );
     markUncompleteButton.addEventListener("click", () => {
       markBookAsUncompleted(id);
     });
@@ -96,7 +118,7 @@ function renderBooks() {
       uncompletedBookList.append(bookElement);
     }
   }
-}
+}   
 // Fungsi untuk menambahkan buku baru
 function addBook(event) {
   event.preventDefault();
@@ -124,24 +146,25 @@ function addBook(event) {
 
   // Reset text tombol submit
   const submitButton = document.getElementById("bookFormSubmit");
-  submitButton.innerHTML = `Masukkan buku ke rak <span class="text-gray-200">Belum selesai dibaca</span>>`;
+  submitButton.innerHTML = `Masukkan buku ke rak <span class="text-rose-200">Belum selesai dibaca</span>`;
 }
 
 // Event listener untuk tombol submit
 document.getElementById("bookForm").addEventListener("submit", addBook);
 
 // Event listener untuk checkbox selesai dibaca
-document.getElementById("bookFormIsComplete").addEventListener("change", (event) => {
-  const isCompleted = event.target.checked;
-  const submitButton = document.getElementById("bookFormSubmit");
+document
+  .getElementById("bookFormIsComplete")
+  .addEventListener("change", (event) => {
+    const isCompleted = event.target.checked;
+    const submitButton = document.getElementById("bookFormSubmit");
 
-  if (isCompleted) {
-    submitButton.innerHTML = `Masukkan buku ke rak <span>Selesai dibaca</span>`;
-  } else {
-    submitButton.innerHTML = `Masukkan buku ke rak <span>Belum selesai dibaca</span>`;
-  }
-});
-
+    if (isCompleted) {
+      submitButton.innerHTML = `Masukkan buku ke rak <span>Selesai dibaca</span>`;
+    } else {
+      submitButton.innerHTML = `Masukkan buku ke rak <span>Belum selesai dibaca</span>`;
+    }
+  });
 
 // Fungsi untuk menghapus buku
 function removeBook(id) {
@@ -176,6 +199,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("bookFormYear");
   const yearDropdown = document.createElement("select");
   yearDropdown.className = input.className;
+  yearDropdown.id = input.id;
+  yearDropdown.required = true;
+  yearDropdown.setAttribute("data-testid", input.getAttribute("data-testid"));
 
   const currentYear = new Date().getFullYear();
   for (let year = 1900; year <= currentYear; year++) {
